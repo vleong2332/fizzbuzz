44e1dda355f4afe55bc48f3d960370952aba6369
VICKY LEONG <VLEONG2332@GMAIL.COM>
2015-03-23 15:58:17 -0500

    Add run fizzbuzz on enter

--- js/refractored.js
+++ js/refractored.js
@@ -1,42 +1,53 @@
-$(document).ready(function() {
-	console.log("Document is ready");
+// MAIN FUNCTION THAT WILL HANDLE USER INPUT
+function runFizzBuzz() {
+	var input = document.getElementById("maxNumber").value;
+	var inputInt = parseInt(input);
+	if (!isNaN(inputInt) && (inputInt % 1 == 0))
+	{
+		refresh();
+		fizzbuzzMe(inputInt);
+		document.getElementById("maxNumber").value = "";
+		// QUESTION: Why can't this work with input = ""??
+	}
+	else
+	{
+		document.getElementById("errorMsg").innerHTML = "Invalid input. Enter whole numbers only.";
+		// QUESTION: What's the difference between innerHTML and nodeValue??
+	}
+}
 
-	// WHEN CLICKING THE GO BUTTON...
-	$('#submitMaxNumber').click( function() {
-		
-		var input = $('#maxNumber');
-
-		if (!isNaN(input.val()) && (input.val() % 1 != 0))
-		{
-			refresh();
-			fizzbuzzMe(input.val());
-			input.val("");
-		}
-		else
-		{
-			$('#errorMsg').text("Invalid input. Enter whole numbers only.");
-		}
-	});
-
-});
 
 // BRING EVERYTHING BACK TO NORMAL
 function refresh() {
-	$('ul#fizzbuzz > li').remove();
-	$('#maxNumber').focus();
-	$('#errorMsg').text("");
+	// Delete everything on the list
+	var element = document.getElementById("fizzbuzz");
+	while (element.firstChild)
+	{
+		element.removeChild(element.firstChild);
+	}
+	// Remove error message, if any
+	document.getElementById("errorMsg").innerHTML = "";
+	// Focus on the Max Number user input
+	document.getElementById("maxNumber").focus();
 };
+
 
 // PRINT FIZZBUZZ UNTIL USER-SUBMITTED NUMBER
 function fizzbuzzMe(maxNum) {
-	var entry;
+	// Loop until we reach user-submitted number
 	for(var i = 1; i <= maxNum; i++)
 	{
-		entry = "<li>";
-		if(i % 3 == 0) entry += "Fizz";
-		if(i % 5 == 0) entry += "Buzz";
-		if((i % 3 != 0) && (i % 5 != 0)) entry += i;
-		entry += "</li>";
-		$('ul#fizzbuzz').append(entry);
+		// Prepare elements to be added
+		var list = document.createElement("li");
+		var entry = document.createTextNode("");
+
+		// Determine the content of the element
+		if(i % 3 == 0) entry.nodeValue += "Fizz";
+		if(i % 5 == 0) entry.nodeValue += "Buzz";
+		if((i % 3 != 0) && (i % 5 != 0)) entry.nodeValue = i;
+
+		// Append text to the list, append list to the UL
+		list.appendChild(entry);
+		document.getElementById("fizzbuzz").appendChild(list);
 	}
 };
